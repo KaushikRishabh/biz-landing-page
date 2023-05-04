c7d2d89a76e893b819a952cc24d20950d146b930
KaushikRishabh <reachmeatrish@gmail.com>
2023-05-04 18:58:17 +0530

    changes

--- pages/components/Post.js
+++ pages/components/Post.js
@@ -1,4 +1,13 @@
-const Post = ({ title, content, image, timestamp }) => {
+const Post = ({ title, content, image, video, timestamp, buttons }) => {
+    // const formattedContent = { __html: content.replace(/\n/g, '<br />') };
+
+    const buyToken = () => {
+        window.open(
+          "https://app.uniswap.org/#/swap?outputCurrency=0xb1a9b52407d1f373a625bc5aa4fe618a886681e5&chain=mainnet",
+          "_blank"
+        );
+      };
+      
     return (
       <div className="post">
         <div className="post-info">
@@ -7,12 +16,48 @@ const Post = ({ title, content, image, timestamp }) => {
           <span> No. 123456</span>
         </div>
         <div className="post-content-wrapper">
-          {image && (
-            <img src={image} alt="Post related" className="post-image" />
-          )}
+            {video ? (
+            <video src={video} alt="Post related" className="post-video" controls />
+            ) : (
+            image && <img src={image} alt="Post related" className="post-image" />
+            )}
           <div className="post-text">
             <h3 className="post-title">{title}</h3>
-            <p className="post-content">{content}</p>
+              {Array.isArray(content) ? (
+                <ul className="post-content-list">
+                    {content.map((item, index) => (
+                    <li key={index} className="post-content-item">
+                        {item}
+                    </li>
+                    ))}
+                </ul>
+                ) : (
+                <p className="post-content">{content}</p>
+                )}
+            {buttons && (
+          <div className="post-buttons">
+            {/* {buttons.map((button, index) => ( */}
+              <button key={buttons[0]} className="post-button" onClick={() => {
+                window.open(
+                    "https://app.uniswap.org/#/swap?outputCurrency=0x39dFD50C197f9288D21A308758DBd080d2Ae8128&chain=mainnet",
+                    "_blank"
+                  );
+              }}>
+                {buttons[0].text}
+              </button>
+              <a
+              href="https://www.dextools.io/app/en/ether/pair-explorer/0x39dFD50C197f9288D21A308758DBd080d2Ae8128"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: "none" }}
+            >
+              <button variant="outlined" color="primary">
+              {buttons[1].text}
+              </button>
+            </a>
+            {/* ))} */}
+          </div>
+        )}
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,12 @@ import Head from 'next/head';
 import Post from './components/Post';
 
 export default function Home() {
+
+  const copyEmail = () => {
+    navigator.clipboard.writeText("example@example.com");
+    alert("Email copied to clipboard!");
+  };
+
   return (
     <div>
       <Head>
@@ -12,7 +18,7 @@ export default function Home() {
 
       <header>
         <div className="container">
-          <h1>/biz/</h1>
+          <h1>/biz/ - biz coin will flip bits-coin</h1>
           <nav>
             {/* <a href="#">Biz</a>
             <a href="#">Tech</a>
@@ -26,31 +32,58 @@ export default function Home() {
             <Post
               title="$biz Contract Address:"
               content="0x39dFD50C197f9288D21A308758DBd080d2Ae8128"
-              image="https://via.placeholder.com/300"
+              image="/sminem.jpg"
+              // video="/mp41.mp4"
               timestamp="05/04/2023"
+              buttons={[
+                { text: 'Buy Now', onClick: () => console.log('Buy Now clicked') },
+                { text: 'View Chart', onClick: () => console.log('View Chart clicked') },
+              ]}
             />
             <Post
-              title="Post Title 2"
+              title="Who is biz For?"
               content="Anon&apos;s /biz/coin - the ultimate crypto kek for financial frens & market memers alike!"
               timestamp="05/04/2023"
             />
             <Post
-              title="Post Title 3"
-              content="Sed et urna semper, blandit turpis a, convallis mauris."
-              image="https://via.placeholder.com/300"
+              title="Tokenomics:"
+              content={[
+                "Total Supply: 420,420,420,420 $biz",
+                "89% of the supply was sent to the Uniswap liquidity pool and then Locked",
+                "11% supply is reserved for future CEX listings and giveaways so $bizcan grow and more bizards can join",
+                "'Tis time for the biz to rise!",
+              ]}
+              image="/img1.jpg"
               timestamp="05/04/2023"
             />
             <Post
-              title="Post Title 4"
-              content="Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
+              title="Roadmap:"
+              content={[
+                "Phase 1: Launch and Build Community of 1000 $biz holders ",
+                "Phase 2: Small CEX listings, and $biz in the nooz",
+                "Biz Labs incorporation and Tier 1 CEX listings",
+                "No irl generational wealth meetups?",
+              ]}
+              image="/logo.png"
+              timestamp="05/04/2023"
+            />
+             <Post
+              title="Post Title 5"
+              content="Sed et urna semper, blandit turpis a, convallis mauris."
+              image="/wojak.jpg"
               timestamp="05/04/2023"
             />
           </div>
         </main>
 
       <footer>
-        <div className="container">
-          <p>&copy; 2023 4chan Clone</p>
+        <div className="footer">
+        <p>
+        © biz coin - 2023 |{" "}
+        <span className="footer-email" onClick={copyEmail}>
+        bizcoin69@proton.me
+        </span>
+      </p>
         </div>
       </footer>
     </div>
